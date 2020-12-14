34d19f940ad2e9b9385d4f041c4009f5aab75706
Adam Kidder <thekidder@gmail.com>
2020-12-13 21:43:19 -0800

    14 part 2 2

--- day14.py
+++ day14.py
@@ -5,12 +5,14 @@ mem = {}
 def run(filename):
   with open(filename, 'r') as f:
     onesmask = 0
-    zerosmask = 68719476735
+    notxmask = 0
+    xmask = []
     for line in f:
       line = line.strip()
       if line.startswith('mask = '):
         onesmask = 0
-        zerosmask = 0
+        notxmask = 0
+        xmask = []
         mask = line[-36:]
         print(mask)
         for i in range(len(mask)):
@@ -18,9 +20,12 @@ def run(filename):
           if bit == '1':
             # print(f'1 bit at {i}')
             onesmask += 1 << i
-          if bit == '0':
+          if bit != 'X':
             # print(f'0 bit at {i}')
-            zerosmask += 1 << i
+            notxmask += 1 << i
+          else:
+            xmask.append(i)
+
         # print(onesmask)
         # print(zerosmask)
       else:
@@ -28,10 +33,16 @@ def run(filename):
         g = m.groups()
         addr = int(g[0])
         val = int(g[1])
-        # print(val & zerosmask)
 
-        mem[addr] = (val | onesmask) ^ (val & zerosmask)
-        print(addr, mem[addr])
+        maskaddr = (addr | onesmask) & notxmask
+        all_addr(mem, val, maskaddr, xmask)
+        # for i in range(68719476735):
+        #   if (i & notxmask) == (maskaddr & notxmask):
+        #     mem[i] = val
+        
+
+        # mem[addr] = (val | onesmask) ^ (val & zerosmask)
+        # print(addr, mem[addr])
 
   s = 0
   for addr, val in mem.items():
@@ -39,6 +50,17 @@ def run(filename):
   print(mem)
   print(s)
   # NOT 9889493670975
+
+def all_addr(mem, val, base, bits):
+  if len(bits) == 0:
+    return
+  bits = bits[:]
+  bit = bits.pop()
+  mem[base] = val
+  mem[base | (1 << bit)] = val
+  all_addr(mem, val, base, bits)
+  all_addr(mem, val, base | (1 << bit), bits)
+
 
 # run('day14_ex.txt')
 run('day14.txt')
