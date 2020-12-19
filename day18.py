1dfbd02d8632c86ae6ec24faaa8fe378d0eecaf5
Adam Kidder <thekidder@gmail.com>
2020-12-19 10:47:01 -0800

    day18: replace stupidly-smart parser with a smartly-stupid one

--- day18.py
+++ day18.py
@@ -1,111 +1,60 @@
 import collections
 import math
 
-def next(expr):
-  expr = expr.strip()
-  if len(expr) > 0:
-    return expr[0]
-  else:
-    return ''
-
 def calc(expr, indent = ''):
   print(f'{indent}CALC {expr}')
+
   i = 0
-  res = 0
-  val = 0
-  op = '+'
+  # first simplify parens
   while i < len(expr):
     token = expr[i]
-    if len(token.strip()) == 0:
-      pass
-    elif token >= '0' and token <= '9':
-      val = int(token)
-      print(f'{indent}res = {res}, val = {val}, op = {op}')
-      if op == '*':
-        cnt = 0
-        j = i+1
-        while j < len(expr):
-          if expr[j] == ' ':
-            pass
-          elif expr[j] == '(':
-            cnt += 1
-          elif expr[j] == ')':
-            cnt -= 1
-          if expr[j] in ['*', ')'] and cnt == 0 and j == next(expr[j:]) != '+':
-            if expr[j] == ')':
-              j += 1
-            break
-          j += 1
-        print(f'{indent}MULT SUBEXPR: {expr[i+1:j]}')
-        val = calc(expr[i+1:j], indent + '  ')
-        res *= val
-        i = j
-        print(f'{indent}MULT after subexpr: {res}')
-      else:
-        res += val
-    elif token in ['*','+']:
-      op = token
-      if op == '*':
-        cnt = 0
-        j = i+1
-        while j < len(expr):
-          if expr[j] == ' ':
-            pass
-          elif expr[j] == '(':
-            cnt += 1
-          elif expr[j] == ')':
-            cnt -= 1
-          if expr[j] in ['*', ')'] and cnt == 0 and j == next(expr[j:]) != '+':
-            if expr[j] == ')':
-              j += 1
-            break
-          j += 1
-        print(f'{indent}MULT SUBEXPR: {expr[i+1:j]}')
-        val = calc(expr[i+1:j], indent + '  ')
-        res *= val
-        i = j
-        print(f'{indent}MULT after subexpr: {res}')
-
-    elif token == '(':
-      cnt = 0
-      for j in range(i, len(expr)):
+    if token == '(':
+      j = i+1
+      cnt = 1
+      while j < len(expr) and cnt != 0:
         if expr[j] == '(':
           cnt += 1
         elif expr[j] == ')':
           cnt -= 1
-          if cnt == 0:
-            val = calc(expr[i+1:j], indent + '  ')
-            if op == '*':
-              res *= val
-            else:
-              res += val
-            i = j
-            print(f'{indent}after subexpr: {res}')
-            break
-    elif token == ')':
-      print('ERROR')
-    else:
-      print('ERROR')
+        j += 1
+      expr = expr[:i] + [calc(expr[i+1:j-1], indent + '  ')] + expr[j:]
     i += 1
 
-  # if token == '*':
-  #   res *= val
-  # else:
-  #   res += val
+  # next do addition
+  i = 0
+  while i < len(expr):
+    token = expr[i]
+    if token == '+':
+      expr = expr[:i-1] + [int(expr[i-1]) + int(expr[i+1])] + expr[i+2:]
+    else:
+      i += 1
 
-  print(f'{indent}CALC return {res}')
-  return res
+  # finally, mult
+  i = 0
+  while i < len(expr):
+    token = expr[i]
+    if token == '*':
+      expr = expr[:i-1] + [int(expr[i-1]) * int(expr[i+1])] + expr[i+2:]
+    else:
+      i += 1
+
+  print(f'{indent}CALC return {expr[0]}')
+  return int(expr[0])
+
+
+def tokenize(expr):
+  expr = expr.strip().replace(' ', '')
+  return [t for t in expr]
+
 
 def run(filename):
   sum = 0
   with open(filename, 'r') as f:
     for line in f:
-      line = line.strip()
-      r = calc(line)
+      r = calc(tokenize(line))
       sum += r
+      print(r)
 
   print(sum)
-# 
-# print(calc('4 + (5 + (5 * 5 + 3 + 2) + (6 + 4 * 9 * 2 * 8) * 6 + (7 * 5 * 2) * (2 * 8 * 2)) + (8 * 7 + 7) * 6 * 9 * (5 + 9)'))
 
 run('day18.txt')
