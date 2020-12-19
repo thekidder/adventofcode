e9d1582c5be90ccb616181798483f1df57b31ad1
Adam Kidder <thekidder@gmail.com>
2020-12-19 09:13:56 -0800

    day 19 cleanup

--- day19.py
+++ day19.py
@@ -1,75 +1,60 @@
-# def valid(msg, rulemap):
-  # pass
-
-v = set()
-rulemap = {}
+valid_msgs = set()
+rules = {}
 msgs = []
 all_prefixes = set()
 
-def generate(rulelist, prefix=''):
-  # print(f'GEN {prefix} {rulelist}')
+def generate(remaining_rules, prefix=''):
   if prefix not in all_prefixes:
-    # print('  (quit)')
     return
-  if len(rulelist) == 0:
+  if len(remaining_rules) == 0:
     yield prefix
   else:
-    first = rulelist[0]
-    rule = rulemap[first]
+    first = remaining_rules[0]
+    rule = rules[first]
       
-    if not isinstance(rule, list):
-      # print('leaf ' + rule)
-      for m in generate(rulelist[1:], prefix + rule):
-        yield m
-    else:
+    if isinstance(rule, list):
       for possibility in rule:
-        # print(f'{rulelist[1:]} +++ {possibility}')
-        for m in generate(possibility + rulelist[1:], prefix):
+        for m in generate(possibility + remaining_rules[1:], prefix):
           yield m
+    else:
+      for m in generate(remaining_rules[1:], prefix + rule):
+        yield m
 
 def run(filename):
   global msgs
   with open(filename, 'r') as f:
     lines = f.read()
     c = lines.split('\n\n')
-    rules = c[0].split('\n')
+    rule_strs = c[0].split('\n')
     msgs = [m.strip() for m in c[1].split('\n')]
 
-    for r in rules:
+    for r in rule_strs:
       c = r.split(':')
       idx = int(c[0].strip())
       rule = c[1].strip()
       if '"' in rule:
-        rulemap[idx] = rule.replace('"','')
+        rules[idx] = rule.replace('"','')
       else:
-        rulemap[idx] = [list(map(int, r.strip().split(' '))) for r in rule.strip().split('|')]
-    print(rulemap)
-    print(msgs)
+        rules[idx] = [list(map(int, r.strip().split(' '))) for r in rule.strip().split('|')]
 
     longest_len = 0
     for m in msgs:
       if len(m) > longest_len:
         longest_len = len(m)
 
+    # generate all possible permutations of prefixes of the messages
     all_prefixes.add('')
     for i in range(1, longest_len+1):
       for m in msgs:
         if len(m) >= i:
           all_prefixes.add(m[0:i])
 
-
-
     for m in generate([0]):
-      v.add(m)
-    print('aaabbbbbbaaaabaababaabababbabaaabbababababaaa' in all_prefixes)
-
-    print(len(v))
-    print('~~~~')
+      valid_msgs.add(m)
 
     cnt = 0
     for m in msgs:
-      if m in v:
-        print(m)
+      if m in valid_msgs:
         cnt += 1
     print(cnt)
 
