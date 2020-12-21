379eff9ccdb814383f9f086273aa1834533dd6d2
Adam Kidder <thekidder@gmail.com>
2020-12-20 22:10:13 -0800

    day 21 cleanups

--- day21.py
+++ day21.py
@@ -1,10 +1,11 @@
 import collections
+import functools
 import re
+
 
 def run(filename):
   ingredients = set()
   ingredient_count = collections.defaultdict(int)
-  allergen_count = collections.defaultdict(int)
   allergens = {}
   with open(filename, 'r') as f:
     for line in f:
@@ -14,44 +15,19 @@ def run(filename):
       if m is None:
         print('ERROR')
       item_ingredients = set([i.strip() for i in g[0].split(' ')])
+      item_allergens = [a.strip() for a in g[1].split(',')]
       for i in item_ingredients:
         ingredient_count[i] += 1
       ingredients |= item_ingredients
-      item_allergens = [a.strip() for a in g[1].split(',')]
-      # if 'peanuts' in item_allergens:
-      # print(item_ingredients)
-      # print(item_allergens)
-      for a in item_allergens:
-        allergen_count[a] += 1
       for allergen in item_allergens:
         if allergen in allergens:
-          if allergen == 'soy':
-            print(f'before {allergens[allergen]}')
           allergens[allergen] &= item_ingredients
-          if allergen == 'soy':
-            print(f'after {allergens[allergen]}')
         else:
-          print(f'set {allergen} to {item_ingredients}')
           allergens[allergen] = set(item_ingredients)
-      if 'soy' in item_allergens:
-        print(f'after loop {allergens["soy"]}')
 
-    print(f'after for {allergens["soy"]}')
-
-
-  print(allergens['soy'])
-  # print(ingredients)
-  print(allergens)
-  print(allergen_count)
-
-  allergen_ingredients = set()
-  for a in allergens.values():
-    allergen_ingredients |= a
-  cnt = 0
-  for i in ingredients - allergen_ingredients:
-    cnt += ingredient_count[i]
-  print(cnt)
-
+  allergen_ingredients = functools.reduce(lambda a, b: a | b, allergens.values())
+  safe_ingredient_count = functools.reduce(lambda a, b: a + ingredient_count[b], ingredients - allergen_ingredients, 0)
+  print(safe_ingredient_count)
 
   done = False
 
