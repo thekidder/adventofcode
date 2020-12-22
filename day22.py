988e498c3258f02780ae7d8d34ca3ef48e2d7a44
Adam Kidder <thekidder@gmail.com>
2020-12-21 21:44:16 -0800

    Fancy python things in day22

--- day22.py
+++ day22.py
@@ -1,7 +1,9 @@
 import functools
 
+debug = False
+
 def roundhash(game):
-  return ','.join(map(str, game[0])) + ':' + ','.join(map(str, game[1]))
+  return ':'.join([','.join([str(c) for c in p]) for p in game])
 
 def winnerof(cards):
   if cards[0] > cards[1]:
@@ -15,32 +17,28 @@ def endround(game, winner, cards):
 
 def scoreof(game):
   def playerscore(p):
-    score = 0
-    for i, c in enumerate(reversed(p)):
-      score += (i + 1) * c
-    return score
+    return functools.reduce(lambda a, p: a + (p[0]+1)*p[1], enumerate(reversed(p)), 0)
   return functools.reduce(lambda a, p: a + playerscore(p), game, 0)
 
 def draw(game):
   return [x.pop(0) for x in game]
 
-def part2_game(game):
-  print(f'START GAME: {game}')
+def part2_game(game, root = True):
+  debug and print(f'START GAME: {game}')
   prevrounds = set()
-  while len(game[0]) > 0 and len(game[1]) > 0:
+  while all(len(p) > 0 for p in game):
     state = roundhash(game)
-    # print(state)
     if state in prevrounds:
       return 0
     prevrounds.add(state)
     cards = draw(game)
-    if len(game[0]) >= cards[0] and len(game[1]) >= cards[1]:
-      winner = part2_game([game[0][:cards[0]], game[1][:cards[1]]])
+    if all(len(g) >= c for g, c in zip(game, cards)):
+      winner = part2_game([g[:c] for g, c in zip(game, cards)], root = False)
       endround(game, winner, cards)
     else:
       endround(game, winnerof(cards), cards)
 
-  print(f'SCORE: {scoreof(game)}')
+  (debug or root) and print(f'Part 2: {scoreof(game)}')
   if len(game[0]) > 0:
     return 0
   return 1
@@ -49,23 +47,19 @@ def run(filename):
   with open(filename, 'r') as f:
     data = f.read()
     players = data.split('\n\n')
-    print(players)
 
     part1 = []
     part2 = []
 
     for p in players:
-      cards = p.split('\n')
-      cards = filter(lambda x: not x.startswith('Player'), cards)
-      cards = list(map(int, cards))
+      cards = [int(c) for c in p.split('\n') if not c.startswith('Player')]
       part1.append(cards[:])
       part2.append(cards[:])
 
-    while len(part1[0]) > 0 and len(part1[1]) > 0:
+    while all(len(p) > 0 for p in part1):
       cards = draw(part1)
       endround(part1, winnerof(cards), cards)
-    print(scoreof(part1))
-
+    print(f'Part 1: {scoreof(part1)}')
     part2_game(part2)
 
 # run('day22_ex.txt')
