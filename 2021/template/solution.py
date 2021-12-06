266fd6502144dc763f47077dfb55494104f0e3e8
Adam Kidder <thekidder@gmail.com>
2021-12-05 16:41:22 -0800

    add template boilerplate

---
+++ 2021/template/solution.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+import re
+import math
+import sys
+
+# regex example
+# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
+# m = line_pattern.match(line)
+# x = int(m.group(1)) # 0 is the entire capture group
+
+def parse_file(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(int(line))
+
+    return lines
+
+
+def part1(filename):
+    ans = 0
+    print(f'ANSWER: {ans}')
+
+
+def part2(filename):
+    pass
+
+
+part1('example.txt')
