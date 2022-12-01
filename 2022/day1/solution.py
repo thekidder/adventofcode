7f4c38df27a3c18a969cf957c9d6a995fdaf9fef
Adam Kidder <thekidder@gmail.com>
2022-11-30 21:35:48 -0800

    try and predict some reused code for the future

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -1,17 +1,14 @@
 import functools
+from helpers import *
 
 def parse_file(filename):
-    with open(filename, 'r') as f:
-        return [
-            [x for x in map(int, l.split('\n'))] 
-            for l in f.read().split('\n\n')
-        ]
+    return grouped_input(filename, int)
 
 def solution(filename):
-    cals = [functools.reduce(lambda a,b: a+b, x) for x in parse_file(filename)]
+    cals = [functools.reduce(add, x) for x in parse_file(filename)]
     cals.sort()
     print(cals[-1])
-    print(functools.reduce(lambda a,b: a+b, cals[-3:]))
+    print(functools.reduce(add, cals[-3:]))
 
 solution('example.txt')
 solution('input.txt')
