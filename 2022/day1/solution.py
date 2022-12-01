8ff4bb3005a8211b473f72361a7a7b643dd05755
Adam Kidder <thekidder@gmail.com>
2022-11-30 21:39:01 -0800

    remember how to python

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -1,14 +1,13 @@
-import functools
 from helpers import *
 
 def parse_file(filename):
     return grouped_input(filename, int)
 
 def solution(filename):
-    cals = [functools.reduce(add, x) for x in parse_file(filename)]
+    cals = [sum(x) for x in parse_file(filename)]
     cals.sort()
     print(cals[-1])
-    print(functools.reduce(add, cals[-3:]))
+    print(sum(cals[-3:]))
 
 solution('example.txt')
 solution('input.txt')
