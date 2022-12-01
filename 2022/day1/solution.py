fec195355c9bb11e769c81958cbd23f7477b0b69
Adam Kidder <thekidder@gmail.com>
2022-11-30 21:08:28 -0800

    day 1

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -3,6 +3,7 @@ from collections import defaultdict, Counter
 import re
 import math
 import sys
+import functools
 
 # regex example
 # pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
@@ -10,22 +11,48 @@ import sys
 # x = int(m.group(1)) # 0 is the entire capture group
 
 def parse_file(filename):
-    lines = []
+    elves = []
+    elf = []
     with open(filename, 'r') as f:
         for line in f:
-            lines.append(int(line))
+            line = line.strip()
+            if len(line) == 0:
+                elves.append(elf)
+                elf = []
+            else:
+                elf.append(int(line))
+    elves.append(elf)
 
-    return lines
+    return elves
 
 
 def part1(filename):
-    input = parse_file(filename)
-    ans = 0
-    print(f'ANSWER: {ans}')
+    elves = parse_file(filename)
+    m = 0
+    for elf in elves:
+        total = functools.reduce(lambda a,b: a+b, elf)
+        print(total)
+        if total > m:
+            m = total
+
+    print(f'ANSWER: {m}')
 
 
 def part2(filename):
-    pass
+    elves = parse_file(filename)
+    totals = []
+    for elf in elves:
+        total = functools.reduce(lambda a,b: a+b, elf)
+        totals.append(total)
+
+    totals.sort()
+    totals = totals[-3:]
+    ans = functools.reduce(lambda a,b: a+b, totals)
 
 
-part1('example.txt')
+
+    print(f'ANSWER: {ans}')
+
+
+# part2('example.txt')
+part2('input.txt')
