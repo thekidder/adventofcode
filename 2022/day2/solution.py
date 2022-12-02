d79b984e54947a5ded9e7b980c6c8fd50512963c
Adam Kidder <thekidder@gmail.com>
2022-12-01 21:24:47 -0800

    garbage day 2 solution

--- 2022/day2/solution.py
+++ 2022/day2/solution.py
@@ -1,39 +1,63 @@
-from collections import defaultdict, Counter
-
-import functools
-import math
-import re
-import sys
-
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
-
 def parse_file(filename):
     lines = []
     with open(filename, 'r') as f:
         for line in f:
-            lines.append(int(line))
+            lines.append(line.split())
 
     return lines
 
-    # group by newlines
-    # return grouped_input(filename, int)
 
+def sc(round):
+    if ord(round[0]) - ord('A') == ord(round[1]) - ord('X'):
+        return 3
+    if round[1] == 'X' and round[0] == 'C':
+        return 6
+    if round[1] == 'Y' and round[0] == 'A':
+        return 6
+    if round[1] == 'Z' and round[0] == 'B':
+        return 6
+    return 0
 
 
 def part1(filename):
+    print(filename)
+    score = 0
     input = parse_file(filename)
-    ans = 0
-    print(f'ANSWER: {ans}')
+    for round in input:
+        score += (ord(round[1]) - ord('X')) + 1
+        score += sc(round)
+
+    print(f'ANSWER: {score}\n')
+
+
+def sc2(round):
+    if round[1] == 'Y':
+        return (ord(round[0]) - ord('A') + 1) + 3
+    if round[1] =='X':
+        if round[0] == 'A':
+            return 3
+        if round[0] == 'B':
+            return 1
+        if round[0] == 'C':
+            return 2
+
+    if round[0] == 'A':
+        return 2 + 6
+    if round[0] == 'B':
+        return 3 + 6
+    if round[0] == 'C':
+        return 1 + 6
 
 
 def part2(filename):
-    pass
+    print(filename)
+    score = 0
+    input = parse_file(filename)
+    for round in input:
+        score += sc2(round)
+
+    print(f'ANSWER: {score}\n')
 
 
-part1('example.txt')
-# part1('input.txt')
+part2('example.txt')
+part2('input.txt')
