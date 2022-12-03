a7a7544c791400dae1d86d8538fcd0b37d8c7209
Adam Kidder <thekidder@gmail.com>
2022-12-02 21:13:10 -0800

    day 3

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -16,7 +16,10 @@ def parse_file(filename):
     lines = []
     with open(filename, 'r') as f:
         for line in f:
-            lines.append(int(line))
+            line = line.strip()
+            a = line[:len(line)//2]
+            b = line[len(line)//2:]
+            lines.append([a,b])
 
     return lines
 
@@ -25,17 +28,54 @@ def parse_file(filename):
 
 
 
+def parse_file2(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            lines.append(line)
+
+    return lines
+
+def priority(item):
+    if item <= 'Z':
+        return ord(item) - ord('A') + 27
+    else:
+        return ord(item) - ord('a') + 1
+
 def part1(filename):
     print(filename)
     input = parse_file(filename)
     ans = 0
+    for c in input:
+        [a, b] = c
+        a_set = set(a)
+        b_set = set(b)
+        for item in a_set:
+            if item in b_set:
+                ans += priority(item)
     print(f'ANSWER: {ans}')
 
 
 def part2(filename):
     print(filename)
-    pass
+    input = parse_file2(filename)
+    ans = 0
+    while len(input) > 0:
+        group = input[:3]
+        a_set = set(group[0])
+        b_set = set(group[1])
+        c_set = set(group[2])
+
+        for item in a_set:
+            if item in b_set and item in c_set:
+                ans += priority(item)
+                break
 
 
-part1('example.txt')
-part1('input.txt')
+        input = input[3:]
+    print(f'ANSWER: {ans}')
+
+
+part2('example.txt')
+part2('input.txt')
