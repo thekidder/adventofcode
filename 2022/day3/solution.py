6b3160d300912e934147cfec27b8d4114eea247c
Adam Kidder <thekidder@gmail.com>
2022-12-02 21:15:36 -0800

    day 3 cleanup

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -1,34 +1,4 @@
-from collections import defaultdict, Counter
-
-import functools
-import math
-import re
-import sys
-
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
-
 def parse_file(filename):
-    lines = []
-    with open(filename, 'r') as f:
-        for line in f:
-            line = line.strip()
-            a = line[:len(line)//2]
-            b = line[len(line)//2:]
-            lines.append([a,b])
-
-    return lines
-
-    # group by newlines
-    # return grouped_input(filename, int)
-
-
-
-def parse_file2(filename):
     lines = []
     with open(filename, 'r') as f:
         for line in f:
@@ -37,29 +7,28 @@ def parse_file2(filename):
 
     return lines
 
+
 def priority(item):
     if item <= 'Z':
         return ord(item) - ord('A') + 27
     else:
         return ord(item) - ord('a') + 1
 
+
 def part1(filename):
-    print(filename)
     input = parse_file(filename)
     ans = 0
-    for c in input:
-        [a, b] = c
-        a_set = set(a)
-        b_set = set(b)
+    for line in input:
+        a_set = set(line[:len(line)//2])
+        b_set = set(line[len(line)//2:])
         for item in a_set:
             if item in b_set:
                 ans += priority(item)
-    print(f'ANSWER: {ans}')
+    print(f'P1 {filename}: {ans}')
 
 
 def part2(filename):
-    print(filename)
-    input = parse_file2(filename)
+    input = parse_file(filename)
     ans = 0
     while len(input) > 0:
         group = input[:3]
@@ -72,10 +41,11 @@ def part2(filename):
                 ans += priority(item)
                 break
 
-
         input = input[3:]
-    print(f'ANSWER: {ans}')
+    print(f'P2 {filename}: {ans}')
 
 
+part1('example.txt')
+part1('input.txt')
 part2('example.txt')
 part2('input.txt')
