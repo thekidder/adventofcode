fb583b126e5909d6da6a6dc3fb3cbe4de6a99d3b
Adam Kidder <thekidder@gmail.com>
2022-12-09 20:56:23 -0800

    2019 day 6 p1 and part of p2

---
+++ 2019/day6/solution.py
@@ -0,0 +1,76 @@
+from collections import defaultdict, Counter
+
+import functools
+import math
+import re
+import sys
+
+
+# regex example
+# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
+# m = line_pattern.match(line)
+# x = int(m.group(1)) # 0 is the entire capture group
+
+def parse_file(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip().split(')'))
+
+    return lines
+
+
+def norbits(orbits, body, stop = None):
+    n = 0
+    while body in orbits and body != stop:
+        n += 1
+        body = orbits[body]
+    return n
+
+
+def part1(filename):
+    input = parse_file(filename)
+    orbits = {}
+    bodies = set()
+    for body,satellite in input:
+        orbits[satellite] = body
+        bodies.add(body)
+        bodies.add(satellite)
+    ans = 0
+    for b in bodies:
+        ans += norbits(orbits,b)
+    print(f'P1 {filename}: {ans}')
+
+
+def transitive_orbits(orbits, body, satellite):
+    while satellite in orbits    and satellite != body:
+        satellite = orbits[satellite]
+    return satellite == body
+
+
+def part2(filename):
+    input = parse_file(filename)
+    orbits = {}
+    bodies = set()
+    for body,satellite in input:
+        orbits[satellite] = body
+        bodies.add(body)
+        bodies.add(satellite)
+
+    dest = orbits['SAN']
+    s = None
+    n = 0
+    for b in bodies:
+        if transitive_orbits(orbits, b, dest) and transitive_orbits(orbits, b, 'YOU') and norbits(orbits, b) > n:
+            n = norbits(orbits, b)
+            s  = b
+
+    ans = norbits(orbits, 'YOU', b)
+    print(f'P2 {filename}: {ans}')
+    
+
+# part1('example.txt')
+# part1('input.txt')
+
+part2('example2.txt')
+# part2('input.txt')
