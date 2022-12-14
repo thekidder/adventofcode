520f9603f35ef1197886178a66bf1f6eafdb1ec0
Adam Kidder <thekidder@gmail.com>
2022-12-14 08:14:51 -0800

    clean up day 14 solution

--- 2022/day14/solution.py
+++ 2022/day14/solution.py
@@ -1,20 +1,6 @@
-from collections import defaultdict, Counter
+def sign(n):
+    return (n > 0) - (n < 0)
 
-import functools
-import itertools
-import math
-import re
-import sys
-
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
-
-def sign(x):
-    if x == 0: return 0
-    return x // abs(x)
 
 def parse_file(filename):
     r = {}
@@ -23,93 +9,66 @@ def parse_file(filename):
         lines = f.readlines()
         for l in lines:
             coords = list([[int(x) for x in x.split(',')] for x in l.split('->')])
-            for i in range(1, len(coords), 1):
+            for i in range(1, len(coords)):
                 prev_x, prev_y = coords[i-1]
                 x, y = coords[i]
+                r[(prev_x, prev_y)] = True
                 while prev_x != x or prev_y != y:
-                    r[(prev_x, prev_y)] = True
                     prev_x += sign(x - prev_x)
                     prev_y += sign(y - prev_y)
                     max_y = max(prev_y, max_y)
                     r[(prev_x, prev_y)] = True
 
-
         return r, max_y
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
-
-    # return lines
 
 
-def n(c):
+def falls_to(c):
     yield (c[0], c[1]+1)
     yield (c[0]-1, c[1]+1)
     yield (c[0]+1, c[1]+1)
 
-def part1(filename):
-    m,max_y = parse_file(filename)
-    print(m)
-    src = (500, 0)
-    ans = 0
-    while True:
-        sand = src
-        while True:
-            moving = False
-            for nsand in n(sand):
-                if nsand not in m:
-                    sand = nsand
-                    moving = True
-                    break
-            # print(f'sim step {sand}')
-            if not moving:
-                if sand not in m:
-                    print(f'resting at {sand}')
-                    m[sand] = True
-                    ans += 1
-                    break
-                # else:
-                #     print(f'P1 {filename}: {ans}')
-                #     sys.exit(0)
 
-            if sand[1] > max_y:
-                print(f'P1 {filename}: {ans}')
-                sys.exit(0)
+def step(m, coord, max_y):
+    def valid(coord):
+        return coord not in m and coord[1] <= max_y
+    return next(filter(valid, falls_to(coord)), None)
+
+
+def simulate(m, coord, max_y, floor):
+    while coord is not None:
+        prev = coord
+        coord = step(m, prev, max_y)
+    if not floor and prev[1] == max_y:
+        return 0
+    if prev not in m:
+        m[prev] = True
+        return 1
+    return 0
+
+
+# def simulate(m, src, max_y, floor):
+#     coord = sim(m, src, max_y)
     
 
 
+def part1(filename):
+    m,max_y = parse_file(filename)
+    ans = 0
+    while simulate(m, (500, 0), max_y, False):
+        ans += 1
+    print(f'P1 {filename}: {ans}')
+    
+
 def part2(filename):
     m,max_y = parse_file(filename)
-    floor = max_y + 2
-    print(m)
-    src = (500, 0)
     ans = 0
-    while src not in m:
-        sand = src
-        while True:
-            moving = False
-            for nsand in n(sand):
-                if nsand not in m and sand[1] != floor-1:
-                    sand = nsand
-                    moving = True
-                    break
-            # print(f'sim step {sand}')
-            if not moving:
-                if sand not in m:
-                    print(f'resting at {sand}')
-                    m[sand] = True
-                    ans += 1
-                    break
-                # else:
-                #     print(f'P1 {filename}: {ans}')
-                #     sys.exit(0)
-
-    print(f'P1 {filename}: {ans}')
+    while simulate(m, (500, 0), max_y+1, True):
+        ans += 1
+    print(f'P2 {filename}: {ans}')
 
 
-# part1('example.txt')
-# part1('input.txt')
+part1('example.txt')
+part1('input.txt')
 
-# part2('example.txt')
+part2('example.txt')
 part2('input.txt')
