510ac1f2cd4ec687f8bcf21173e3c659ee942494
Adam Kidder <thekidder@gmail.com>
2022-12-17 21:56:05 -0800

    clean up day 18

--- 2022/day18/solution.py
+++ 2022/day18/solution.py
@@ -1,17 +1,8 @@
-from collections import defaultdict, Counter
-
-import functools
-import itertools
-import math
-import re
+from collections import defaultdict
+import operator
 import sys
 
 from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
 
 def parse_file(filename):
     r = []
@@ -23,17 +14,22 @@ def parse_file(filename):
         return r
 
 
+def populate_edges(edges, coord):
+    edges[vadd(coord, (-1,0,0)) + ('X',)] += 1
+    edges[vadd(coord, (0,-1,0)) + ('Y',)] += 1
+    edges[vadd(coord, (0,0,-1)) + ('Z',)] += 1
+
+    edges[coord + ('X',)] += 1
+    edges[coord + ('Y',)] += 1
+    edges[coord + ('Z',)] += 1
+
+
 def part1(filename):
     input = parse_file(filename)
     edges = defaultdict(int)
     for coord in input:
-        edges[(coord[0], coord[1], coord[2], 'X')] += 1
-        edges[(coord[0], coord[1], coord[2], 'Y')] += 1
-        edges[(coord[0], coord[1], coord[2], 'Z')] += 1
-
-        edges[(coord[0]-1, coord[1], coord[2], 'X')] += 1
-        edges[(coord[0], coord[1]-1, coord[2], 'Y')] += 1
-        edges[(coord[0], coord[1], coord[2]-1, 'Z')] += 1
+        populate_edges(edges, coord)
+        
     ans = 0
     for edge in edges.values():
         if edge == 1:
@@ -47,54 +43,37 @@ def part2(filename):
 
     filled = {x: True for x in input}
 
-    xs = (sys.maxsize, 0)
-    ys = (sys.maxsize, 0)
-    zs = (sys.maxsize, 0)
+    min_coord = (sys.maxsize,sys.maxsize,sys.maxsize)
+    max_coord = (0,0,0)
 
     for coord in input:
-        xs = (min(xs[0], coord[0]), max(xs[1], coord[0]))
-        ys = (min(ys[0], coord[1]), max(ys[1], coord[1]))
-        zs = (min(zs[0], coord[2]), max(zs[1], coord[2]))
-        edges[(coord[0], coord[1], coord[2], 'X')] += 1
-        edges[(coord[0], coord[1], coord[2], 'Y')] += 1
-        edges[(coord[0], coord[1], coord[2], 'Z')] += 1
+        min_coord = tuple(map(min, min_coord, coord))
+        max_coord = tuple(map(max, max_coord, coord))
 
-        edges[(coord[0]-1, coord[1], coord[2], 'X')] += 1
-        edges[(coord[0], coord[1]-1, coord[2], 'Y')] += 1
-        edges[(coord[0], coord[1], coord[2]-1, 'Z')] += 1
-
+        populate_edges(edges, coord)
     ans = 0
 
     memo = {}
     def is_inside(c):
+        if c in memo and memo[c]:
+            return True
         q = [c]
         all = set()
         while len(q) > 0:
             c = q.pop()
-            # print(c)
-            # print(f'check {c}')
-            if c not in filled:
-                if c in memo:
-                    if memo[c]:
-                        break
-                    else:
-                        # for c in all:
-                        #     memo[c] = False
-                        return False
+            if c not in filled:                
+                if c in memo and memo[c]:
+                    break
                 all.add(c)
-                if c[0] <= xs[0] or c[0] >= xs[1] or \
-                    c[1] <= ys[0] or c[1] >= ys[1] or \
-                    c[2] <= zs[0] or c[2] >= zs[1]:
-                    # for c in all:
-                    #     memo[c] = False
+                if any(map(operator.le, c, min_coord)) or any(map(operator.ge, c, max_coord)):
                     return False
                 for n in [
-                    (c[0]-1, c[1], c[2]),
-                    (c[0], c[1]-1, c[2]),
-                    (c[0], c[1], c[2]-1),
-                    (c[0]+1, c[1], c[2]),
-                    (c[0], c[1]+1, c[2]),
-                    (c[0], c[1], c[2]+1),
+                    vadd(c, (-1,0,0)),
+                    vadd(c, (0,-1,0)),
+                    vadd(c, (0,0,-1)),
+                    vadd(c, (1,0,0)),
+                    vadd(c, (0,1,0)),
+                    vadd(c, (0,0,1)),
                 ]:
                     if n not in all and n not in filled:
                         q.append(n)
@@ -102,25 +81,14 @@ def part2(filename):
             memo[c] = True
         return True
 
-    print(xs,ys,zs)
-    for x in range(xs[0], xs[1]+1):
-        for y in range(ys[0], ys[1]+1):
-            for z in range(zs[0], zs[1]+1):
+    for x in range(min_coord[0], max_coord[0]+1):
+        for y in range(min_coord[1], max_coord[1]+1):
+            for z in range(min_coord[2], max_coord[2]+1):
                 coord = (x,y,z)
                 if coord in filled:
                     continue
-                # print(x,y,z)
                 if is_inside(coord):
-                    edges[(coord[0], coord[1], coord[2], 'X')] += 10
-                    edges[(coord[0], coord[1], coord[2], 'Y')] += 10
-                    edges[(coord[0], coord[1], coord[2], 'Z')] += 10
-
-                    edges[(coord[0]-1, coord[1], coord[2], 'X')] += 10
-                    edges[(coord[0], coord[1]-1, coord[2], 'Y')] += 10
-                    edges[(coord[0], coord[1], coord[2]-1, 'Z')] += 10
-
-    print(len(memo))
-    print(len(filled))
+                    populate_edges(edges, coord)
 
     for edge in edges.values():
         if edge == 1:
@@ -129,8 +97,8 @@ def part2(filename):
     print(f'P2 {filename}: {ans}')
 
 
-# part1('example.txt')
-# part1('input.txt')
+part1('example.txt')
+part1('input.txt')
 
 part2('example.txt')
 part2('input.txt')
