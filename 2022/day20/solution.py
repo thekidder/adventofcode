eef6a2963050ec0098d5ecd79db3734f5f1a60a9
Adam Kidder <thekidder@gmail.com>
2022-12-19 21:28:58 -0800

    clean up day 20

--- 2022/day20/solution.py
+++ 2022/day20/solution.py
@@ -1,60 +1,16 @@
-from collections import defaultdict, Counter
-
-import functools
-import itertools
-import math
-import re
-import sys
-
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
-
-def parse_file(filename):
+def parse_file(filename, key):
     r = []
     with open(filename, 'r') as f:
-        return list(map(int, f.readlines()))
+        return list(map(lambda l: int(l)*key, f.readlines()))
 
 
-def part1(filename):
-    input = parse_file(filename)
+def solve(filename, iterations, key):
+    input = parse_file(filename, key)
     indices = list(range(len(input)))
     
-    for x in range(len(input)):
-        ind = indices.index(x)
-        val = input.pop(ind)
-
-        next_ind = (ind + val) % len(input)
-        if val != 0 and next_ind == 0:
-            next_ind = len(input)
-        elif next_ind == len(input):
-            next_ind = 0
-
-        input.insert(next_ind, val)
-
-        indices.pop(ind)
-        indices.insert(next_ind, x)
-
-        # print(f'{val} moves from {ind}, {ind + val} to {next_ind}: {input}')
-    
-    ind = input.index(0)
-    ans = input[(ind + 1000) % len(input)] + input[(ind + 2000) % len(input)] + input[(ind + 3000) % len(input)]
-    print(f'P1 {filename}: {ans}')
-
-
-def part2(filename):
-    input = list(map(lambda x: x * 811589153, parse_file(filename)))
-    indices = list(range(len(input)))
-    
-    print(input)
-    for i in range(10):
-        orig = input[:]
+    for _ in range(iterations):
         for x in range(len(input)):
             ind = indices.index(x)
-            # ind = input.index(orig[x])
             val = input.pop(ind)
 
             next_ind = (ind + val) % len(input)
@@ -67,17 +23,16 @@ def part2(filename):
 
             indices.pop(ind)
             indices.insert(next_ind, x)
-
-        # print(f'{val} moves from {ind}, {ind + val} to {next_ind}: {input}')
-        print(input)
     
     ind = input.index(0)
-    ans = input[(ind + 1000) % len(input)] + input[(ind + 2000) % len(input)] + input[(ind + 3000) % len(input)]
-    print(f'P2 {filename}: {ans}')
+    ans = input[(ind + 1000) % len(input)] + \
+        input[(ind + 2000) % len(input)] + \
+        input[(ind + 3000) % len(input)]
+    print(f'Mix {filename} {iterations} time(s): {ans}')
 
 
-# part1('example.txt')
-# part1('input.txt')
+# solve('example.txt', 1, 1)
+solve('input.txt', 1, 1)
 
-# part2('example.txt')
-part2('input.txt')
+# solve('example.txt', 10, 811589153)
+solve('input.txt', 10, 811589153)
