aa4dbcb9262986a28112e0169bb79cdf849e8af3
Adam Kidder <thekidder@gmail.com>
2022-12-22 21:44:47 -0800

    day 22 solution

--- 2022/day23/solution.py
+++ 2022/day23/solution.py
@@ -14,25 +14,113 @@ from helpers import *
 # x = int(m.group(1)) # 0 is the entire capture group
 
 def parse_file(filename):
-    r = []
+    r = defaultdict(bool)
     with open(filename, 'r') as f:
-        lines = f.read()
-        sections = lines.split('\n\n')
-
+        lines = f.readlines()
+        for y,line in enumerate(lines):
+            for x,c in enumerate(line):
+                if c == '#':
+                    r[(x,y)] = True
         return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
 
-    # return lines
 
+dirs = [
+    (0, -1),
+    (0, 1),
+    (-1, 0),
+    (1, 0),
+]
+
+def adjacent(r, pos):
+    for x in range(-1, 2):
+        for y in range(-1,2):
+            if x == 0 and y == 0: continue
+            npos = vadd(pos, (x,y))
+            if npos in r and r[npos]: return True
+    return False
+
+
+def can_move(r, pos, dir):
+    if dir[1] != 0:
+        return not any([vadd(pos, (x, dir[1])) in r and r[vadd(pos, (x, dir[1]))] for x in range(-1, 2)])
+    return not any([vadd(pos, (dir[0], y)) in r and r[vadd(pos, (dir[0], y))] for y in range(-1, 2)])
+
+
+def printmap(m):
+    print('STATE:')
+    minb = [sys.maxsize, sys.maxsize]
+    maxb = [0, 0]
+    for space,elf in m.items():
+        if not elf: continue
+        minb[0] = min(minb[0], space[0])
+        minb[1] = min(minb[1], space[1])
+
+        maxb[0] = max(maxb[0], space[0])
+        maxb[1] = max(maxb[1], space[1])
+    
+    for y in range(minb[1], maxb[1]+1):
+        for x in range(minb[0], maxb[0]+1):
+            if (x,y) in m and m[(x,y)]:
+                print('#',end='')
+            else:
+                print('.',end='')
+        print()
+ 
 
 def part1(filename):
-    input = parse_file(filename)
-    print(input)
-    ans = 0
-    print(f'P1 {filename}: {ans}')
+    m = parse_file(filename)
+    for round in range(10000):
+        proposals = defaultdict(int)
+        actions = {}
+        # printmap(m)
+        for space,elf in m.items():
+            if not elf: continue
+            if not adjacent(m, space):
+                actions[space] = space
+            else:
+                for d in dirs:
+                    if can_move(m, space, d):
+                        next = vadd(space, d)
+                        # print(f'{space} MOVES {d} TO {next}')
+                        actions[space] = next
+                        proposals[next] += 1
+                        break
+                else:
+                    actions[space] = space
+                    # print(f'{space} CANT MOVE {d}')
+
+        n = defaultdict(bool)
+        has_moves = False
+        for prev,next in actions.items():
+            if prev == next or proposals[next] > 1:
+                n[prev] = True
+            else:
+                has_moves = True
+                n[next] = True
+
+        if not has_moves:
+            print(f'ENDED AT ROUND {round+1}')
+            sys.exit(1)
+        m = n
+
+        d = dirs.pop(0)
+        dirs.append(d)
+
+    elves = 0
+    minb = [sys.maxsize, sys.maxsize]
+    maxb = [0, 0]
+    for space,elf in m.items():
+        if not elf: continue
+        elves += 1
+        minb[0] = min(minb[0], space[0])
+        minb[1] = min(minb[1], space[1])
+
+        maxb[0] = max(maxb[0], space[0])
+        maxb[1] = max(maxb[1], space[1])
+
+    ans = (maxb[0] - minb[0]+1) * (maxb[1] - minb[1]+1) - elves
+
+    print(f'P1 {filename}: {maxb} {minb} {ans}')
 
 
 def part2(filename):
@@ -41,8 +129,8 @@ def part2(filename):
     print(f'P2 {filename}: {ans}')
 
 
-part1('example.txt')
-# part1('input.txt')
+# part1('example.txt')
+part1('input.txt')
 
 # part2('example.txt')
 # part2('input.txt')
