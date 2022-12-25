ff8a9e8413d293df688ef07bd4bedf3f4a03e2b5
Adam Kidder <thekidder@gmail.com>
2022-12-25 05:46:07 +0000

    trash code for day 25

--- 2022/day25/solution.py
+++ 2022/day25/solution.py
@@ -14,35 +14,78 @@ from helpers import *
 # x = int(m.group(1)) # 0 is the entire capture group
 
 def parse_file(filename):
-    r = []
     with open(filename, 'r') as f:
-        lines = f.read()
-        sections = lines.split('\n\n')
+        return f.readlines()
 
-        return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
+def to_decimal_digit(d):
+    if d == '2': return 2
+    if d == '1': return 1
+    if d == '0': return 0
+    if d == '-': return -1
+    return -2
 
-    # return lines
+def to_decimal(snafu):
+    s = 0
+    for i,digit in enumerate(reversed(snafu.strip())):
+        s += pow(5, i) * to_decimal_digit(digit)
+    return s
 
+
+chars = ['=','-','0','1','2']
+
+def dst(s,d): return abs(to_decimal(''.join(s))-d) 
+
+def to_snafu(s):
+    n = []
+    while to_decimal(''.join(n)) < 2*s:
+        n = ['2'] + n
+    for i in range(len(n)):
+        md = sys.maxsize
+        mc = None
+        for c in chars:
+            n[i] = c
+            val = to_decimal(''.join(n))
+            print(f'try {c} at {i} for {dst(n, s)} {val}')
+            if dst(n, s) < md:
+                md = dst(n, s)
+                mc = c
+        n[i] = mc
+        num = ''.join(n)
+        print(f'{n} {to_decimal(num)}')
+    while to_decimal(''.join(n)) < s:
+        for i in range(len(n)-1,-1,-1):
+            if n[i] != '2':
+                n[i] = chars[chars.index(n[i])+1]
+                break
+        else:
+            n = ['1'] + n
+        print(f'fix digit at {i} to {n[i]}')
+        for j in range(i+1, len(n)):
+            md = sys.maxsize
+            mc = None
+            for c in chars:
+                n[j] = c
+                val = to_decimal(''.join(n))
+                # print(f'try {c} at {j} for {dst(n, s)} {val}')
+                if dst(n, s) < md:
+                    md = dst(n, s)
+                    mc = c
+            n[j] = mc
+            num = ''.join(n)
+            print(f'{n} {to_decimal(num)}')
+        
+    return ''.join(n)
+        
 
 def part1(filename):
     input = parse_file(filename)
-    print(input)
-    ans = 0
-    print(f'P1 {filename}: {ans}')
+    ans = sum(map(to_decimal, input))
+    print(f'P1 {filename}: {ans} {to_snafu(ans)}')
 
 
-def part2(filename):
-    input = parse_file(filename)
-    ans = 0
-    print(f'P2 {filename}: {ans}')
 
-
-part1('example.txt')
-# part1('input.txt')
+part1('example.txt') # 4890 2=-1=0
+part1('input.txt')
 
 # part2('example.txt')
 # part2('input.txt')
