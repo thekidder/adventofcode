9abf46133d5d029c4587701e3f8a95efe05eef43
Adam Kidder <thekidder@gmail.com>
2023-11-30 21:11:01 -0800

    day 1

--- 2023/day01/solution.py
+++ 2023/day01/solution.py
@@ -29,20 +29,35 @@ def parse_file(filename):
 
 
 def part1(filename):
-    input = parse_file(filename)
-    print(input)
     ans = 0
+    with open(filename, 'r') as f:
+        for l in f:
+            nums = [x for x in l if x >='0' and x <= '9']
+            ans += int(nums[0] + nums[-1])
     print(f'P1 {filename}: {ans}')
 
 
 def part2(filename):
-    input = parse_file(filename)
     ans = 0
-    print(f'P2 {filename}: {ans}')
+    with open(filename, 'r') as f:
+        for l in f:
+            l = l.replace('one', 'o1e')
+            l = l.replace('two', 't2o')
+            l = l.replace('three', 't3e')
+            l = l.replace('four', 'f4r')
+            l = l.replace('five', 'f5e')
+            l = l.replace('six', 's6x')
+            l = l.replace('seven', 's7n')
+            l = l.replace('eight', 'e8t')
+            l = l.replace('nine', 'n9e')
+            nums = [x for x in l if x >= '0' and x <= '9']
+            print(int(nums[0] + nums[-1]))
+            ans += int(nums[0] + nums[-1])
+    print(f'P1 {filename}: {ans}')
 
 
-part1('example.txt')
-# part1('input.txt')
+part2('example2.txt')
+part2('input.txt')
 
 # part2('example.txt')
 # part2('input.txt')
