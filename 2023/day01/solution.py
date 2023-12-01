49e0d4debf3785a455490dccb6782935f56d326b
Adam Kidder <thekidder@gmail.com>
2023-11-30 21:12:43 -0800

    cleanup

--- 2023/day01/solution.py
+++ 2023/day01/solution.py
@@ -1,33 +1,3 @@
-from collections import defaultdict, Counter
-
-import functools
-import itertools
-import math
-import re
-import sys
-
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
-
-def parse_file(filename):
-    r = []
-    with open(filename, 'r') as f:
-        lines = f.read()
-        sections = lines.split('\n\n')
-
-        return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
-
-    # return lines
-
-
 def part1(filename):
     ans = 0
     with open(filename, 'r') as f:
@@ -51,9 +21,8 @@ def part2(filename):
             l = l.replace('eight', 'e8t')
             l = l.replace('nine', 'n9e')
             nums = [x for x in l if x >= '0' and x <= '9']
-            print(int(nums[0] + nums[-1]))
             ans += int(nums[0] + nums[-1])
-    print(f'P1 {filename}: {ans}')
+    print(f'P2 {filename}: {ans}')
 
 
 part2('example2.txt')
