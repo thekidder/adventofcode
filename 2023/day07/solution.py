2258f7891e87a231e125383b06f9bf4cda9846af
Adam Kidder <thekidder@gmail.com>
2023-12-06 21:34:27 -0800

    day 7 solution

--- 2023/day07/solution.py
+++ 2023/day07/solution.py
@@ -1,48 +1,117 @@
-from collections import defaultdict, Counter
+from collections import defaultdict
 
-import functools
-import itertools
-import math
-import re
-import sys
-
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
 
 def parse_file(filename):
-    r = []
+    lines = []
     with open(filename, 'r') as f:
-        lines = f.read()
-        sections = lines.split('\n\n')
+        for line in f:
+            hand, bid = line.split()
+            lines.append((hand.strip(), int(bid)))
 
-        return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
+    return lines
 
-    # return lines
+
+val = {
+    'A': 12, 
+    'K': 11, 
+    'Q': 10, 
+    'J': 9, 
+    'T': 8, 
+    '9': 7, 
+    '8': 6, 
+    '7': 5, 
+    '6': 4, 
+    '5': 3, 
+    '4': 2, 
+    '3': 1,
+    '2': 0
+}
+
+val_j = {
+    'A': 12, 
+    'K': 11, 
+    'Q': 10, 
+    'T': 9, 
+    '9': 8, 
+    '8': 7, 
+    '7': 6, 
+    '6': 5, 
+    '5': 4, 
+    '4': 3, 
+    '3': 2,
+    '2': 1,
+    'J': 0 
+}
+
+
+def ords(hand, wildj = False):
+    cnt = defaultdict(int)
+    jcnt = 0
+    for c in hand:
+        if not wildj or c != 'J':
+            cnt[c] += 1
+        elif wildj:
+            jcnt += 1
+    return sorted(cnt.values(), reverse = True), jcnt
+
+
+def has(hand, n, m = None, wildj = False):
+    cnt, jcnt = ords(hand, wildj = wildj)
+    if len(cnt) == 0 and wildj:
+        return jcnt == n
+    if wildj:
+        cnt[0] += jcnt
+    if m is None:
+        return cnt[0] == n
+    return cnt[0] == n and cnt[1] == m
+
+
+def hand_key(hand, wildj):
+    v = 0
+    hand = hand[0]
+    if wildj:
+        vals = val_j
+    else:
+        vals = val
+    for i, c in enumerate(reversed(hand)):
+        v += vals[c] * 13**(i)
+
+    if has(hand, 5, wildj=wildj):
+        v += 6*500000
+    elif has(hand, 4, wildj=wildj):
+        v += 5*500000
+    elif has(hand, 3, 2, wildj=wildj):
+        v += 4*500000
+    elif has(hand, 3, wildj=wildj):
+        v += 3*500000
+    elif has(hand, 2, 2, wildj=wildj):
+        v += 2*500000
+    elif has(hand, 2, wildj=wildj):
+        v += 500000
+
+    return v
 
 
 def part1(filename):
     input = parse_file(filename)
-    print(input)
+    input = sorted(input, key = lambda h: hand_key(h, False))
     ans = 0
+    for i, h in enumerate(input):
+        ans += (i+1) * h[1]
     print(f'P1 {filename}: {ans}')
 
 
 def part2(filename):
     input = parse_file(filename)
+    input = sorted(input, key = lambda h: hand_key(h, True))
     ans = 0
-    print(f'P2 {filename}: {ans}')
+    for i, h in enumerate(input):
+        ans += (i+1) * h[1]
+    print(f'P1 {filename}: {ans}')
 
 
 part1('example.txt')
-# part1('input.txt')
+part1('input.txt')
 
-# part2('example.txt')
-# part2('input.txt')
+part2('example.txt')
+part2('input.txt')
