0ab67f146dcc6ad0af67998652e3983861c025b3
Adam Kidder <thekidder@gmail.com>
2023-12-10 20:53:44 -0800

    update template

--- 2023/template/helpers.py
+++ 2023/template/helpers.py
@@ -7,12 +7,23 @@ def file(name):
 
 def parse_grid(filename):
     r = {}
+    mx = 0
+    my = 0
     with open(filename, 'r') as f:
         for (y, l) in enumerate(f):
+            my = y
             for (x, c) in enumerate(l.strip()):
                 r[(x,y)] = c
+                mx = x
 
-        return r
+        return r,mx,my
+
+
+def print_grid(m, mx, my):
+    for y in range(my):
+        for x in range(mx):
+            print(m[(x,y)] if (x,y) in m else '.', end = '')
+        print()
 
 
 def neighbors(coord):
@@ -36,7 +47,7 @@ def vsub(a, b):
 
 
 def vmul(a, b):
-    return tuple(map(operator.add, a, b))
+    return tuple(map(operator.mul, a, b))
 
 
 def mhn_dist(a, b):
