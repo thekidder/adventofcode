7cd4ae3e7b78d206b27b096c927d602abd9ba806
Adam Kidder <thekidder@gmail.com>
2023-12-12 21:30:45 -0800

    day 13 sol

--- 2023/day13/solution.py
+++ 2023/day13/solution.py
@@ -1,48 +1,62 @@
-from collections import defaultdict, Counter
+def parse_grid(text):
+    r = {}
+    mx = 0
+    my = 0
+    f = text.split('\n')
+    for (y, l) in enumerate(f):
+        my = y
+        for (x, c) in enumerate(l.strip()):
+            r[(x,y)] = c
+            mx = x
 
-import functools
-import itertools
-import math
-import re
-import sys
+    return r,mx,my
 
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
 
 def parse_file(filename):
-    r = []
     with open(filename, 'r') as f:
         lines = f.read()
-        sections = lines.split('\n\n')
-
-        return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
-
-    # return lines
+        grids = lines.split('\n\n')
+        return list(map(parse_grid, grids))
 
 
-def part1(filename):
+def is_mirror_col(grid,mx,my,col,nfaults=0):
+    faults = 0
+    ncols = min(col, mx - col + 1)
+    for i in range(ncols):
+        for y in range(my+1):
+            if grid[(col-i-1,y)] != grid[(col+i,y)]:
+                faults += 1
+    return faults == nfaults
+
+
+def is_mirror_row(grid,mx,my,row,nfaults=0):
+    faults = 0
+    nrows = min(row, my - row + 1)
+    for i in range(nrows):
+        for x in range(mx+1):
+            if grid[(x,row-i-1)] != grid[(x,row+i)]:
+                faults += 1
+    return faults == nfaults
+
+
+def pattern(input, nfaults):
+    grid,mx,my = input
+    for col in range(1, mx+1):
+        if is_mirror_col(grid,mx,my,col,nfaults):
+            return col
+    for row in range(1, my+1):
+        if is_mirror_row(grid,mx,my,row,nfaults):
+            return row * 100
+    return 0
+
+def solve(filename):
     input = parse_file(filename)
-    print(input)
-    ans = 0
+    ans = sum(map(lambda i: pattern(i, 0), input))
+    print(f'P1 {filename}: {ans}')
+
+    ans = sum(map(lambda i: pattern(i, 1), input))
     print(f'P1 {filename}: {ans}')
 
 
-def part2(filename):
-    input = parse_file(filename)
-    ans = 0
-    print(f'P2 {filename}: {ans}')
-
-
-part1('example.txt')
-# part1('input.txt')
-
-# part2('example.txt')
-# part2('input.txt')
+solve('example.txt')
+solve('input.txt')
