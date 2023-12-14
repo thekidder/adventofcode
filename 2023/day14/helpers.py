f7c03a8c526c721c5dae6929471c66b63e9e198d
Adam Kidder <thekidder@gmail.com>
2023-12-13 21:33:32 -0800

    day 14 sol

--- 2023/day14/helpers.py
+++ 2023/day14/helpers.py
@@ -1,9 +1,5 @@
 import operator
 
-def file(name):
-    with open(name, 'r') as f:
-        return f.read()
-
 
 def parse_grid(filename):
     r = {}
@@ -26,43 +22,13 @@ def print_grid(m, mx, my):
         print()
 
 
-def transpose(m):
-    r = {}
-    for (x,y), v in m.items():
-        r[(y,x)] = v
-    return r
-
-
-def neighbors(coord):
-    for xd in range(-1, 2):
-        for yd in range(-1, 2):
-            if xd == 0 and yd == 0:
-                continue
-            yield (coord[0] + xd, coord[1] + yd)
-
-
-def sign(n):
-    return (n > 0) - (n < 0)
-
-
 def vadd(a, b):
     return tuple(map(operator.add, a, b))
 
 
-def vsub(a, b):
-    return tuple(map(operator.sub, a, b))
-
-
-def vmul(a, b):
-    return tuple(map(operator.mul, a, b))
-
-
-def mhn_dist(a, b):
-    return sum(map(abs, vsub(a, b)))
-
-# dirs = {
-#     'W': (-1, 0),
-#     'E': (1, 0),
-#     'S': (0, -1),
-#     'N': (0, 1),
-# }
+dirs = {
+    'W': (-1, 0),
+    'E': (1, 0),
+    'N': (0, -1),
+    'S': (0, 1),
+}
--- 2023/day14/solution.py
+++ 2023/day14/solution.py
@@ -1,48 +1,72 @@
-from collections import defaultdict, Counter
-
-import functools
-import itertools
-import math
-import re
-import sys
-
 from helpers import *
 
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
+def roll(input, mx, my, dir):
+    moves = 1
+    while moves > 0:
+        moves = 0
+        for y in range(my+1):
+            for x in range(mx+1):
+                if input[(x,y)] != 'O':
+                    continue
+                n = vadd((x,y), dir)
+                if n in input and input[n] == '.':
+                    input[n] = 'O'
+                    input[(x,y)] = '.'
+                    moves += 1
 
-def parse_file(filename):
-    r = []
-    with open(filename, 'r') as f:
-        lines = f.read()
-        sections = lines.split('\n\n')
 
-        return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
-
-    # return lines
+def load(input, my):
+    r = 0
+    for (x,y), v in input.items():
+        if v == 'O':
+            r += my - y + 1 
+    return r
 
 
 def part1(filename):
-    input = parse_file(filename)
-    print(input)
-    ans = 0
-    print(f'P1 {filename}: {ans}')
+    input,mx,my = parse_grid(filename)
+    # print_grid(input,mx,my)
+    # print()
 
+    roll(input,mx,my, dirs['N'])
+    print(f'P1 {filename}: {load(input,my)}')
+
+
+def has_cycle(prev):
+    # cycle len up to 100
+    for i in range(2,100):
+        if len(prev) < i * 2:
+            continue
+
+        if prev[-i:] == prev[-i*2:-i]:
+            return i
+    return 0
 
 def part2(filename):
-    input = parse_file(filename)
-    ans = 0
+    input,mx,my = parse_grid(filename)
+    # print_grid(input,mx,my)
+    # print()
+
+    i = 0
+    prev = []
+    while not has_cycle(prev):
+        roll(input,mx,my, dirs['N'])
+        roll(input,mx,my, dirs['W'])
+        roll(input,mx,my, dirs['S'])
+        roll(input,mx,my, dirs['E'])
+
+        i += 1
+        
+        prev.append(load(input,my))
+    cycle_len = has_cycle(prev)
+    prev = prev[-cycle_len:]
+    print(f'found cycle of len {cycle_len} after {i} iterations: {prev}')
+    ans = prev[(1000000000 - i - 1) % cycle_len]
     print(f'P2 {filename}: {ans}')
 
 
 part1('example.txt')
-# part1('input.txt')
+part1('input.txt')
 
-# part2('example.txt')
-# part2('input.txt')
+part2('example.txt')
+part2('input.txt')
