336a4f9c964675ac70a4cb8e0c663786794b0cbd
Adam Kidder <thekidder@gmail.com>
2023-12-24 20:56:37 -0800

    day 24 wip

--- 2023/template/solution.py
+++ 2023/template/solution.py
@@ -4,7 +4,12 @@ import functools
 import itertools
 import math
 import re
+import random
 import sys
+
+import numpy as np
+# from scipy import optimize
+from scipy.optimize import fsolve,least_squares
 
 from helpers import *
 
@@ -84,10 +89,60 @@ def part1(filename):
 
 def part2(filename):
     input = parse_file(filename)
-    ans = 0
+    # random.shuffle(input)
+    # print(input)
 
-    for i in itertools.permutations(input, len(input)):
-        print(i)
+    # for v in generate_velocity():
+    #     for i in input[1:]:
+
+    # values = np.array([])
+
+    # bounds = optimize.Bounds(-np.inf, np.inf)
+    # integrality = np.full_like(values, True) 
+
+    n = len(input)
+
+    def f(x):
+        p = x[:3]
+        v = x[3:6]
+        t = x[6:]
+
+        # n_eqns = len(input) + 6 # 11 for example
+        eqns = []
+        for i in range(len(input)): # 1 - 4
+            for j in range(3):
+                eqns.append(t[i]*input[i][1][j] + input[i][0][j] - (t[i]*v[j] + p[j]))
+        # for i in x:
+        #     ip, fp = math.modf(i)
+        #     eqns.append(fp)
+        return eqns#[:n_eqns]
+
+    guess = [input[0][0][0]+1,input[0][0][1]+1,input[0][0][2]+1,1,1,1] + [1]*n#list(range(1, n+1))
+    # guess = [24,13,10, -3,1,2, 5,3,4,6,1]
+    bounds = (
+        # [-np.inf, -np.inf, -np.inf, -1000, -1000, -1000] + [1] * n,
+        # [np.inf, np.inf, np.inf, 1000, 1000, 1000] + [np.inf] * n,
+        [-4000000000000000] * 3 + [-1000000] * 3 + [0] * n,
+        [ 4000000000000000] * 3 + [1000000] * 3 + [10000000000] * n,
+    )
+    res = least_squares(f, guess, bounds=bounds,method='dogbox',jac='cs')
+    print('STATUS', res.success, res.status)
+    # roots = list(map(round, res.x))
+    roots = list(map(round, res.x))
+    # roots = res.x
+    print(roots)
+    # print(list(map(int, roots)))
+    ans = roots[0] + roots[1] + roots[2]
+
+    print(roots[0] + roots[3]*roots[6]) #prx + vrx * t1 
+    print(input[0][0][0] + input[0][1][0]*roots[6]) #p1x + v1x * t1 
+
+
+    # constraints = optimize.LinearConstraint(A=eqns, lb=0, ub=capacity)
+
+    # res = milp(c=-values, constraints=constraints,
+    #        integrality=integrality, bounds=bounds) 
+            
 
     print(f'P2 {filename}: {ans}')
 
@@ -95,5 +150,8 @@ def part2(filename):
 # part1('example.txt')
 # part1('input.txt')
 
-# part2('example.txt')
+part2('example.txt')
+# too high 836739974338902
+# too high 754362884530662
+# too high 754362884532830
 part2('input.txt')
