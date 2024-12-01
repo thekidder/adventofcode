8e634e0401cb71915db4a3243c478a0bcb748c4a
Adam Kidder <thekidder@gmail.com>
2024-11-30 20:44:30 -0800

    more wip for 2019 day 16

--- 2019/day16/solution.py
+++ 2019/day16/solution.py
@@ -30,20 +30,20 @@ def pattern(base, pos, l):
 
 
 # @functools.cache
-def fast_pattern(pos, l, min_i, max_i):
-    # print('FAST PATTERN')
-    skip = min_i - 1 # account for shift
-    if max_i - min_i + 1 > pos:
-        print('ERR')
-    if pos < skip:
-        print('SKIP ERR')
-    l = max_i - min_i + 1
-    if skip + pos < l:
-        print('LEN ERR')
-    # print(f'return {skip+1} - {skip+l}')
-    # return [x for x in range(skip+1, skip+l+1)]
-    for i in range(skip+1, max_i+1):
-        yield i
+# def fast_pattern(pos, l, min_i, max_i):
+#     # print('FAST PATTERN')
+#     skip = min_i - 1 # account for shift
+#     if max_i - min_i + 1 > pos:
+#         print('ERR')
+#     if pos < skip:
+#         print('SKIP ERR')
+#     l = max_i - min_i + 1
+#     if skip + pos < l:
+#         print('LEN ERR')
+#     # print(f'return {skip+1} - {skip+l}')
+#     # return [x for x in range(skip+1, skip+l+1)]
+#     for i in range(skip+1, max_i+1):
+#         yield i
 
 
 def fft(x, base):
@@ -60,6 +60,14 @@ def fft(x, base):
     return x
 
 
+def part1(filename):
+    x = [x for x in digits(file(filename))]
+    x = fft(x, base)
+
+    ans = ''.join(map(str, x[:8]))
+    print(f'P1 {filename}: {ans}')
+
+
 def fast_fft(x, base, min_i, max_i):
     y = [0] * len(x)
     for iter in range(100):
@@ -69,46 +77,55 @@ def fast_fft(x, base, min_i, max_i):
             r = 0
             for j in fast_pattern(i, len(x), min_i, max_i):
                 r += x[j]
-            # for a,b in zip(x, ):
-            #     r += a * b
             y[i] = abs(r) % 10
         x = y
         print(''.join(map(str, x[min_i:min_i+80])))
     return x
 
 
-def part1(filename):
-    x = [x for x in digits(file(filename))]
-    x = fft(x, base)
+# @functools.cache
+def fast_pattern(base, pos, l):
+    p = pattern(base, pos, l)
+    r = []
+    for i,v in enumerate(p):
+        if v != 0:
+            r.append((i,v))
+    return r 
 
-    ans = ''.join(map(str, x[:8]))
-    print(f'P1 {filename}: {ans}')
 
-
+# notes:
+# -- only 1s seem to be present in the pattern
 def part2(filename):
+    # x = [x for x in digits(file(filename))]
+    # skip = 0
     x = [x for x in digits(file(filename))] * 10000
-    # print(len(x))
     skip = int(''.join(map(str, x[:7])))
-    min_i = float('inf')
-    max_i = 0
+    y = x[:]
+    dependent_positions = set()
     for i in range(8):
-        p = pattern(base, skip+i, len(x))
-        n = 0
-        for j, c in enumerate(p):
-            if c != 0:
-                n += 1
-                min_i = min(min_i, j)
-                max_i = max(max_i, j)
-    print(skip, len(p), min_i, max_i, max_i - min_i)
-    # print(skip)
-    x = fft(x, base, min_i, max_i)
+        dependent_positions.add(skip+i)
+        p = fast_pattern(base, skip+i, len(x))
+        for (j,v) in p:
+            dependent_positions.add(j)
+    for iter in range(100):
+        
+        val = ''.join(map(str, x[skip:skip+8]))
+        print(f'iteration {iter}: {val}')
+        for num,i in enumerate(dependent_positions):
+            # print(f'{num}/{len(dependent_positions)}')
+            p = fast_pattern(base, i, len(x))
+            r = 0
+            for (j,v) in p:
+                r += v * x[j]
+            y[i] = abs(r) % 10
+        x = y
 
     ans = ''.join(map(str, x[skip:skip+8]))
     print(f'P2 {filename}: {ans}')
 
 
 # part1('example.txt')
-part1('input.txt')
+# part1('input.txt')
 
-# part2('example.txt')
+part2('example.txt')
 # part2('input.txt')
