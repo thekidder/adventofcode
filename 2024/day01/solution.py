2984cb2986d98d161ea18dc3466875756a822199
Adam Kidder <thekidder@gmail.com>
2024-11-30 21:06:19 -0800

    2024 day 1 solution

--- 2024/day01/solution.py
+++ 2024/day01/solution.py
@@ -1,48 +1,40 @@
-from collections import defaultdict, Counter
-
-import functools
-import itertools
-import math
-import re
-import sys
-
-from helpers import *
-
-# regex example
-# pattern = re.compile('(\d+),(\d+) -> (\d+),(\d+)')
-# m = line_pattern.match(line)
-# x = int(m.group(1)) # 0 is the entire capture group
+from collections import defaultdict
 
 def parse_file(filename):
-    r = []
+    a = []
+    b = []
     with open(filename, 'r') as f:
-        lines = f.read()
-        sections = lines.split('\n\n')
-
-        return r
-    # lines = []
-    # with open(filename, 'r') as f:
-    #     for line in f:
-    #         lines.append(int(line))
-
-    # return lines
+        for line in f:
+            a1,b1 = line.split()
+            a.append(int(a1))
+            b.append(int(b1))
+    return a,b
 
 
 def part1(filename):
-    input = parse_file(filename)
-    print(input)
+    a,b = parse_file(filename)
+    a.sort()
+    b.sort()
     ans = 0
+    for x,y in zip(a,b):
+        ans += abs(x-y)
+
     print(f'P1 {filename}: {ans}')
 
 
 def part2(filename):
-    input = parse_file(filename)
+    a,b = parse_file(filename)
+    cnt_b = defaultdict(int)
+    for val in b:
+        cnt_b[val] += 1
     ans = 0
+    for val in a:
+        ans += val * cnt_b[val]
     print(f'P2 {filename}: {ans}')
 
 
 part1('example.txt')
-# part1('input.txt')
+part1('input.txt')
 
-# part2('example.txt')
-# part2('input.txt')
+part2('example.txt')
+part2('input.txt')
