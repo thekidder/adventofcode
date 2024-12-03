8f6f4966109820bcfb489ab7d8d58ed9722f18a6
Adam Kidder <thekidder@gmail.com>
2024-12-02 21:13:03 -0800

    2024 day 3

---
+++ 2024/day03/solution.py
@@ -0,0 +1,39 @@
+import re
+
+
+pattern = re.compile('mul\((\d+),(\d+)\)')
+pattern2 = re.compile('(mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\))')
+
+def parse_file(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+def part1(filename):
+    input = parse_file(filename)
+    ans = 0
+    for m in pattern.findall(input):
+        ans += int(m[0]) * int(m[1])
+    print(f'P1 {filename}: {ans}')
+
+
+def part2(filename):
+    input = parse_file(filename)
+    ans = 0
+    enabled = True
+    for m in pattern2.findall(input):
+        if m[4] == 'don\'t()':
+            enabled = False
+        elif m[3] == 'do()':
+            enabled = True
+        else:
+            if enabled:
+                ans += int(m[1]) * int(m[2])
+    print(f'P2 {filename}: {ans}')
+
+
+part1('example.txt')
+part1('input.txt')
+
+part2('example2.txt')
+part2('input.txt')
