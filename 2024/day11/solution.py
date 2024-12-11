aa66bf9f4b323bed3c63e7099da464c8801b2e26
Adam Kidder <thekidder@gmail.com>
2024-12-10 21:15:51 -0800

    cleanup day 11

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -5,20 +5,6 @@ def parse_file(filename):
         return list(map(int, f.read().split()))
 
 
-def blink(x):
-    r = []
-    for v in x:
-        if v == 0:
-            r.append(1)
-        elif len(str(v)) % 2 == 0:
-            as_str = str(v)
-            r.append(int(as_str[:len(as_str)//2]))
-            r.append(int(as_str[len(as_str)//2:]))
-        else:
-            r.append(2024*v)
-    return r
-
-
 def blink_dict(x):
     r = defaultdict(int)
     for k,v in x.items():
@@ -33,32 +19,23 @@ def blink_dict(x):
     return r
 
 
-def part1(filename):
-    input = parse_file(filename)
-
-    for _ in range(25):
-        input = blink(input)
-
-    print(f'P1 {filename}: {len(input)}')
-
-
-def part2(filename):
+def solve(filename, i):
     input = parse_file(filename)
     nums = defaultdict(int)
 
     for n in input:
         nums[n] += 1
     
-    for _ in range(75):
+    for _ in range(i):
         nums = blink_dict(nums)
 
     ans = sum(nums.values())
 
-    print(f'P2 {filename}: {ans}')
+    print(f'{filename} after {i} iterations: {ans}')
 
 
-part1('example.txt')
-part1('input.txt')
+solve('example.txt', 25)
+solve('input.txt', 25)
 
-part2('example.txt')
-part2('input.txt')
+solve('example.txt', 75)
+solve('input.txt', 75)
