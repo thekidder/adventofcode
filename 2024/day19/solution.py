eb93c771bb5c0010c74a02c247ae2b8596bf01c5
Adam Kidder <thekidder@gmail.com>
2024-12-18 21:58:51 -0800

    clean up day 19

--- 2024/day19/solution.py
+++ 2024/day19/solution.py
@@ -4,58 +4,36 @@ import functools
 def parse_file(filename):
    with open(filename, 'r') as f:
         lines = f.read()
-        sections = lines.split('\n\n')
-        towels = sections[0].split(',')
-        towels = list(map(lambda x: x.strip(), towels))
-        towels.sort()
-
-        designs = sections[1].split()
-
-        return towels, designs
-
-towels = []
-
-@functools.cache
-def can_make(design, partial = ''):
-    r = 0
-    if design == '':
-        return 1
-    pos = bisect.bisect_left(towels, design[:1])
-    for i in range(pos, len(towels)):
-        t = towels[i]
-        if t > design:
-            break
-        if design[:len(t)] == t:
-             r += can_make(design[len(t):], partial + t)
-    return r
+        towels, designs = lines.split('\n\n')
+        towels = list(map(lambda x: x.strip(), towels.split(',')))
+        return sorted(towels), designs.split()
 
 
-def part1(filename):
-    global towels
+def solve(filename):
     towels, designs = parse_file(filename)
-    ans = 0
+    p1_ans = p2_ans = 0
 
-    for d in designs:
-        if can_make(d, ''):
-            ans += 1
-
-    print(f'P1 {filename}: {ans}')
-
-
-def part2(filename):
-    global towels
-    towels, designs = parse_file(filename)
-    ans = 0
+    @functools.cache
+    def can_make(design, partial = ''):
+        r = 0
+        if design == '':
+            return 1
+        pos = bisect.bisect_left(towels, design[:1])
+        for i in range(pos, len(towels)):
+            t = towels[i]
+            if t > design:
+                break
+            if design[:len(t)] == t:
+                r += can_make(design[len(t):], partial + t)
+        return r
 
     for d in designs:
         r = can_make(d, '')
-        ans += r
+        p1_ans += min(r, 1)
+        p2_ans += r
 
-    print(f'P2 {filename}: {ans}')
+    print(f'{filename}: P1 {p1_ans} P2 {p2_ans}')
 
 
-part1('example.txt')
-part1('input.txt')
-
-part2('example.txt')
-part2('input.txt')
+solve('example.txt')
+solve('input.txt')
